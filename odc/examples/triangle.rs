61d5a79fa687a6be2b27d9e6386f9009f5a3d41a
f3kilo <f3kilo@yandex.ru>
2022-01-05 11:18:36 +0300

    Event loop in example.

--- odc/examples/triangle.rs
+++ odc/examples/triangle.rs
@@ -1,1 +1,28 @@
-fn main() {}
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use odc::{TriangleRenderer, WindowSize};
+
+fn main() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = winit::window::Window::new(&event_loop).unwrap();
+    let size = window.inner_size();
+    let size = WindowSize(size.width, size.height);
+    let mut renderer = TriangleRenderer::new(&window, size);
+    event_loop.run(move |event, _, flow| {
+        *flow = ControlFlow::Wait;
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::Resized(size),
+                ..
+            } => {
+                let size = WindowSize(size.width, size.height);
+                renderer.resize(size);
+            },
+            Event::RedrawRequested(_) => {
+                renderer.render_triangle();
+            }
+            _ => {}
+        }
+    });
+}
--- odc/src/lib.rs
+++ odc/src/lib.rs
@@ -1,9 +1,19 @@
+use raw_window_handle::HasRawWindowHandle;
+
 pub struct TriangleRenderer {}
 
 impl TriangleRenderer {
-    pub fn render_triangle() {
+    pub fn new(window: &impl HasRawWindowHandle, size: WindowSize) -> Self {
+        todo!()
+    }
+
+    pub fn render_triangle(&self) {
+        todo!()
+    }
+
+    pub fn resize(&mut self, size: WindowSize) {
         todo!()
     }
 }
 
-
+pub struct WindowSize(pub u32, pub u32);
