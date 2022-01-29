bb71af3773adeebb746c39686dfb1279d00b59c2
f3kilo <f3kilo@yandex.ru>
2022-01-29 20:40:01 +0300

    Start creating render from structure.

--- odc_core/src/render.rs
+++ odc_core/src/render.rs
@@ -1,6 +1,13 @@
 use crate::{GfxDevice, RenderStructure};
+use structure::Pass;
 
-pub struct RenderData {}
+pub struct RenderData {
+	buffers: HashMap<String, Buffer>,
+	textures: HashMap<String, Texture>,
+	bind_groups: HashMap<String, BindGroup>,
+	pipelines: HashMap<String, Pipeline>,
+	passes: HashMap<String, Pass>
+}
 
 impl RenderData {
 	pub fn from_structure(device: &GfxDevice, structure: &RenderStructure) -> Self {
