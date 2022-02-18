526e741ebf3c842599aa7ff8a2a9656971b10911
f3kilo <f3kilo@yandex.ru>
2022-02-18 12:57:09 +0300

    Run all examples script.

---
+++ run_all_examples.bat
@@ -0,0 +1,5 @@
+start "" /wait cargo run --example triangle
+start "" /wait cargo run --example instances
+start "" /wait cargo run --example deferred
+start "" /wait cargo run --example sprites
+start "" /wait cargo run --example meshes
