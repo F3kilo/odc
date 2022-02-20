6062fe9fe4c05fbde89aa652cee3fef439903fed
f3kilo <f3kilo@yandex.ru>
2022-02-20 11:34:37 +0300

    Run all examples sh scripts.

---
+++ odc_core/run_all_examples_release.sh
@@ -0,0 +1,6 @@
+cargo run --release --example triangle
+cargo run --release --example instances
+cargo run --release --example meshes
+cargo run --release --example deferred
+cargo run --release --example sprites
+cargo run --release --example mip_maps
